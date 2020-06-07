d537ab9f223089e176b3be59c1fd46c8bd766206
socar-oliver <oliver@socar.kr>
2020-06-07 17:01:28 +0900

    protect prisma endpoint

--- woolstagram/src/api/Hello/sayHello/sayHello.js
+++ woolstagram/src/api/Hello/sayHello/sayHello.js
@@ -1,5 +1,10 @@
+import {prisma} from "../../../../generated/prisma-client";
+
 export default {
     Query:{
-        sayHello: () => "Hello"
+        sayHello: async () => {
+            console.log(await prisma.users()); //prisma의 endpoint를 보호할수있다
+            return "Hello";
+        }
     }
 }
